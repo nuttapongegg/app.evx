eb50ca651e0c3578967899f247e63c9d2b384d19
nuttapong <nuttapong.ep@gmail.com>
2024-07-15 17:02:31 +0700

    edit Language

--- app/Views/index.php
+++ app/Views/index.php
@@ -709,7 +709,7 @@
   <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
   <script src="./assets/js/plugins.js"></script>
   <script src="./assets/js/theme.js"></script>
-  <script src="./assets/js/index.js?v=<?= time(); ?>"></script>
+  <script src="./assets/js/index.js"></script>
 </body>
 
 </html>
